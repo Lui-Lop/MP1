469e19623f3d00694beaf897585c8bcb379500ce
Luis <lopezlui@grinnell.edu>
2024-09-11 18:30:01 -0500

    Initial commit

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -6,9 +6,15 @@ import edu.grinnell.csc207.util.CipherUtils;
 public class AllCaesar {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    String str = "helloworld";
-    for (char ch = 'a'; ch <= 'z'; ch++) {
-      pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(str, ch));
+//    String str = "helloworld";
+    if(args[0].equals("encode")){
+      for (char ch = 'a'; ch <= 'z'; ch++) {
+        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
+      }
+    } else if(args[0].equals("denode")){
+      for (char ch = 'a'; ch <= 'z'; ch++) {
+        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(args[1], ch));
+      }
     }
     pen.close();
   }
