dd50d2b6b91090d1e5cda5cbcf122fca1929b9c1
Luis <lopezlui@grinnell.edu>
2024-09-13 19:15:10 -0500

    working!

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -9,42 +9,78 @@ public class Cipher {
     for (int i = 0; i < args.length; i++) {
       pen.printf("args[%d] = \"%s\"\n", i, args[i]);
     }
-    for (int code = 0; code < args.length; code++) {
-      if (args[code].equals("-encode")){
-        for (int cipher = 0; cipher < args.length; cipher++){
-          if (args[cipher].equals("-caesar") ){
-            if (args[3].length() == 1) {
-              pen.println(CipherUtils.caesarEncrypt(args[2], args[3].charAt(0)));
-              return;
+    String cipher = "";
+    String action = "";
+    String str = "";
+    String key = "";
+
+    if (args.length == 4){
+      for (int i = 0; i < args.length; i++) {
+
+        if (args[i].charAt(0) == '-'){
+          if (args[i].equals("-caesar")) {
+            if (cipher.isEmpty()) {
+              cipher = args[i];
             } else {
-              System.err.println("Error: Caesar ciphers require a one-character key");
+              System.err.println("Error: cipher parameter already given");
               return;
             }
-          } else if (args[cipher].equals("-vigenere")) {
-            pen.println(CipherUtils.vigenereEncrypt(args[2], args[3]));
-            return;
+
+          } else if (args[i].equals("-vigenere")) {
+            if (cipher.isEmpty()) {
+              cipher = args[i];
+            } else {
+              System.err.println("Error: cipher parameter already given");
+              return;
+            }
+
+          } else if (args[i].equals("-encode")) {
+            if (action.isEmpty()) {
+              action = args[i];
+            } else {
+              System.err.println("Error: action parameter already give");
+              return;
+            }
+
+          }  else if (args[i].equals("-decode")) {
+            if (action.isEmpty()) {
+              action = args[i];
+            } else {
+              System.err.println("Error: action parameter already give");
+              return;
+            }
+          }
+
+        } else {
+          if (str.isEmpty()) {
+            str = args[i];
+          } else {
+            key = args [i];
           }
         }
 
-      } else if (args[code].equals("-decode")) {
-        for (int cipher = 0; cipher < args.length; cipher++) {
-          if (args[cipher].equals("-caesar")){
-            if (args[3].length() == 1) {
-              pen.println(CipherUtils.caesarDecrypt(args[2], args[3].charAt(0)));
-              return;
-            } else if (!(args[cipher].length()==1)) {
-              System.err.println("Error: Caesar ciphers require a one-character key");
-              return;
-            } // checks if caessar cipher is requested, if so if the key is single letter, else error is given
-          } else if (args[cipher].equals("-vigenere")) {
-            pen.println(CipherUtils.vigenereDecrypt(args[2], args[3]));
-            return;
-          } // checks if argument is -caesar or vigenere
-        } // iterates through args to check for proper arguments
-      } else {
-        System.err.println("Error: No valid action specified. Legal values are '-encode' and '-decode'");
+      }
+    } else {
+      System.err.printf("Error: Expected 4 parameters, received %d\n", args.length);
+    }
+
+    if (cipher.equals("-caesar")) {
+      if (key.length() != 1){
+        System.err.println("Error: caesar cipher takes single character key");
         return;
-      } // checks if command arguments are valid, if not, error is given
-    } // iterates through args to check command line arguments
+      } else if (action.equals("-encode")) {
+        pen.println(CipherUtils.caesarEncrypt(str, key.charAt(0)));
+      } else if (action.equals("-decode")) {
+        pen.println(CipherUtils.caesarDecrypt(str, key.charAt(0)));
+      }
+    } else if (cipher.equals("-vigenere")){
+      if (action.equals("-encode")) {
+        pen.println(CipherUtils.vigenereEncrypt(str, key));
+      } else if (action.equals("-decode")) {
+        pen.println(CipherUtils.vigenereDecrypt(str, key));
+      }
+    }
+    // pen.println(CipherUtils.caesarEncrypt(str, key.charAt(0)));
+    // pen.printf("%S \n%S \n%S \n%S \n", cipher, action, str, key);
   } // end of main
 } // end of class Cipher
