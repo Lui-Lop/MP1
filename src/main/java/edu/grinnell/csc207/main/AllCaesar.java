7850b6421c5cd71ed56579df52847c42d1b3c03f
Luis <lopezlui@grinnell.edu>
2024-09-13 19:52:40 -0500

    style almost perfect

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -7,10 +7,14 @@ import edu.grinnell.csc207.util.CipherUtils;
  * class AllCaesar to take in command line arguments to decrypt/encrypt strings.
  */
 public class AllCaesar {
+    /**
+   * variable to account for 26 lower case letters being used.
+   */
+  private static final int EXPECTED_NUM_PARAMS = 26;
   /**
    *
    * @param args
-   *  command line arguments to be used for caeser ciphering
+   *  takes command line arguments to either encode or decode using string with caesar cipher.
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
@@ -19,26 +23,26 @@ public class AllCaesar {
       return;
     } else {
       String str = args[1];
-      for (int i = 0; i < str.length(); i++){
+      for (int i = 0; i < str.length(); i++) {
         char chr = str.charAt(i);
         int check = (int) chr - (int) 'a';
-        if (check > 26 || check < 0){
+        if (check > EXPECTED_NUM_PARAMS || check < 0) {
           System.err.println("Error: String contains characters other than lowercase letters.");
           return;
-        } // checks if characters are only lower case letters
-      }
+        } // checks if characters are only lower case letters, if not, error is given
+      } // iterates through string to check if only lower case letters
     } //checks if correct number of arguments are passed, if not returns an error else checks if string contains only lower case characters
     if (args[0].equals("encode")) {
       for (char ch = 'a'; ch <= 'z'; ch++) {
         pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
-      }
-    } else if (args[0].equals("decode")){
+      } // takes string and encrypts with each letter of the alphabet
+    } else if (args[0].equals("decode")) {
       for (char ch = 'a'; ch <= 'z'; ch++) {
         pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(args[1], ch));
-      }
+      } // takes string and decrypts with each letter of the alphabet
     } else {
       System.err.println("Error: Invalid Option " + args[0] + ". Valid options are \"Encode\" and \"Encode\" ");
     } // checks if argument passed is valid option, if not error is given
     pen.close();
   } // main where methods are written to be used
-}
+} // end of class
--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -3,84 +3,86 @@ package edu.grinnell.csc207.main;
 import java.io.PrintWriter;
 import edu.grinnell.csc207.util.CipherUtils;
 
+/**
+ * cipher class takes in command line arguments to cipher appropriately.
+ */
 public class Cipher {
+  /**
+   * expected amount of command line arguments.
+   */
+  private static final int EXPECTED_NUM1_PARAMS = 4;
+  /**
+   *
+   * @param args
+   *  command line arguments to tell determine appropriate ciphering.
+   */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    for (int i = 0; i < args.length; i++) {
-      pen.printf("args[%d] = \"%s\"\n", i, args[i]);
-    }
+
     String cipher = "";
     String action = "";
     String str = "";
     String key = "";
 
-    if (args.length == 4){
+    if (args.length == EXPECTED_NUM1_PARAMS) {
       for (int i = 0; i < args.length; i++) {
-
-        if (args[i].charAt(0) == '-'){
+        if (args[i].charAt(0) == '-') {
           if (args[i].equals("-caesar")) {
             if (cipher.isEmpty()) {
               cipher = args[i];
             } else {
               System.err.println("Error: cipher parameter already given");
               return;
-            }
-
+            } // checks if cipher variable is filled, if not set, if yes give error
           } else if (args[i].equals("-vigenere")) {
             if (cipher.isEmpty()) {
               cipher = args[i];
             } else {
               System.err.println("Error: cipher parameter already given");
               return;
-            }
-
+            } // checks if cipher variable is filled, if not set, if yes give error
           } else if (args[i].equals("-encode")) {
             if (action.isEmpty()) {
               action = args[i];
             } else {
               System.err.println("Error: action parameter already give");
               return;
-            }
-
+            } // checks if action variable is filled, if not set, if yes give error
           }  else if (args[i].equals("-decode")) {
             if (action.isEmpty()) {
               action = args[i];
             } else {
               System.err.println("Error: action parameter already give");
               return;
-            }
-          }
-
+            } // checks if action variable is filled, if not set, if yes give error
+          } // checks if command line argument starts with '-' to find proper variable to set
         } else {
           if (str.isEmpty()) {
             str = args[i];
           } else {
-            key = args [i];
-          }
-        }
-
-      }
+            key = args[i];
+          } // checks if str variable is filled, if not fill with string, if yes then fill key
+        } // checks argument to see if action, cipher, string, or key to then be set
+      } // for loop to set command line arguments to appropriate variables
     } else {
       System.err.printf("Error: Expected 4 parameters, received %d\n", args.length);
-    }
+    } // end of main
 
     if (cipher.equals("-caesar")) {
-      if (key.length() != 1){
+      if (key.length() != 1) {
         System.err.println("Error: caesar cipher takes single character key");
         return;
       } else if (action.equals("-encode")) {
         pen.println(CipherUtils.caesarEncrypt(str, key.charAt(0)));
       } else if (action.equals("-decode")) {
         pen.println(CipherUtils.caesarDecrypt(str, key.charAt(0)));
-      }
-    } else if (cipher.equals("-vigenere")){
+      } // checks if action is decode or encode to perform encyption
+    } else if (cipher.equals("-vigenere")) {
       if (action.equals("-encode")) {
         pen.println(CipherUtils.vigenereEncrypt(str, key));
       } else if (action.equals("-decode")) {
         pen.println(CipherUtils.vigenereDecrypt(str, key));
-      }
-    }
-    // pen.println(CipherUtils.caesarEncrypt(str, key.charAt(0)));
-    // pen.printf("%S \n%S \n%S \n%S \n", cipher, action, str, key);
+      } // checks if action is decode or encode to perform encyption
+    } // checks variables 'cipher' and to figure out proper method calls for encryption
   } // end of main
 } // end of class Cipher
